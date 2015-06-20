230e3e9ecd965f4dfaa833d8c21cd0a5dcd2058a
Splash <splash@list.ru>
2015-06-20 14:31:46 +0300

    Keyboard manager

--- Cookies Time/Cookies Time/AppDelegate.m
+++ Cookies Time/Cookies Time/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "IQKeyboardManager.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,24 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    //Enabling keyboard manager
+    [[IQKeyboardManager sharedManager] setEnable:YES];
+    
+    [[IQKeyboardManager sharedManager] setKeyboardDistanceFromTextField:15];
+    //Enabling autoToolbar behaviour. If It is set to NO. You have to manually create IQToolbar for keyboard.
+    [[IQKeyboardManager sharedManager] setEnableAutoToolbar:YES];
+    
+    //Setting toolbar behavious to IQAutoToolbarBySubviews. Set it to IQAutoToolbarByTag to manage previous/next according to UITextField's tag property in increasing order.
+    [[IQKeyboardManager sharedManager] setToolbarManageBehaviour:IQAutoToolbarBySubviews];
+    
+    //Resign textField if touched outside of UITextField/UITextView.
+    [[IQKeyboardManager sharedManager] setShouldResignOnTouchOutside:YES];
+    
+    //Giving permission to modify TextView's frame
+    [[IQKeyboardManager sharedManager] setCanAdjustTextView:YES];
+    
+    //Show TextField placeholder texts on autoToolbar
+    [[IQKeyboardManager sharedManager] setShouldShowTextFieldPlaceholder:YES];
     return YES;
 }
 
